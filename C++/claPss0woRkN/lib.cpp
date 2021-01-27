31c7090f9e7c9268fa207ecce05656118aa0681a
Sergey Doggunovich <70939274+doggunovich68@users.noreply.github.com>
2021-01-27 13:35:37 +0300

    classwork multifile program C++

---
+++ C++/claPss0woRkN/lib.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+void f(){
+    cout<<endl;
+cout<<"Hello world"<<endl;
+}
+
+
+
---
+++ C++/claPss0woRkN/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "lib.h"
+
+using namespace std;
+
+int main()
+{
+    setlocale (LC_ALL, "rus");
+    f();
+    cout <<" project was успешно completed"<<endl;
+
+    return 0;
+}
