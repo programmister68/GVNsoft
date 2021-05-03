a9a7d72cc0d6e18b76ff2379aaa927d4bf2eb728
Sergey Dogganovich <70939274+doggunovich68@users.noreply.github.com>
2021-05-03 14:10:48 +0300

    Create eup_99.cpp

---
+++ C++/Euler_Project/eup_99/eup_99.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+    ifstream file ("Base_exp.txt");
+    int b[5000],b_max=0, index=1, char_number=0;
+    while (!file.eof())
+    {
+        file >> b[index];
+        index++;
+    }
+    for (int i=1; i<index; i=i+2)
+    {
+        if (b[i+1]*log(b[i])> b_max)
+        {
+            b_max = log(b[i])*b[i+1];
+            char_number = (i+1)/2;
+
+        }
+    }
+    cout << "Answer: " << char_number;
+    return 0;
+}
